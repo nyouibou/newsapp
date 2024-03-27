aebd39967158a3621a49dd92aa7c13434abc7364
akshai <57423537+nyouibou@users.noreply.github.com>
2024-03-27 08:58:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:newsapp/controller/homesceencontroller.dart';
+import 'package:newsapp/view/homescreen/homescreen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => HomeScreenController(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
